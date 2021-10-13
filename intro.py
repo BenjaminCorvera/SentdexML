81fbd7c75e5ced5f420acfd995797f1877ffa686
Benjamin Corvera <Benjamin.Corvera@willowtreeapps.com>
2021-10-13 13:48:12 -0400

    finished first linear regression example

--- intro.py
+++ intro.py
@@ -1,8 +1,21 @@
+from more_itertools.more import one
 import pandas as pd
 import quandl as Quandl
-import math
+import math, os, datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+from dotenv import load_dotenv
+from matplotlib import style
+import pickle
 
-df = Quandl.get("WIKI/GOOGL")
+load_dotenv()
+style.use("ggplot")
+
+apiKey = os.getenv("QUANDL_API_KEY")
+
+df = Quandl.get("WIKI/GOOGL", api_key=apiKey)
 df = df[["Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume"]]
 df["HL_PCT"] = (df["Adj. High"] - df["Adj. Close"]) / df["Adj. Close"] * 100
 df["PCT_change"] = (df["Adj. Close"] - df["Adj. Open"]) / df["Adj. Open"] * 100
@@ -13,5 +26,55 @@ forecast_col = "Adj. Close"
 # In ml, you can't work with NaN or null data
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.1*len(df)))
-df['label'] = df[forecast_col].shift(-forecast_out)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df["label"] = df[forecast_col].shift(-forecast_out)
+
+# features
+X = np.array(df.drop(["label"], 1))
+X = preprocessing.scale(X)
+X = X[:-forecast_out]
+
+# stuff we are going to predict against (we don't have a y value for these)
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
+# labels
+y = np.array(df["label"])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+# fit is synonoumous with train
+clf.fit(X_train, y_train)
+with open('linearegression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
+# score is synonomous with test
+accuracy = clf.score(X_test, y_test)
+
+# this is the most important part. This can take a single value or an array
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df["Forecast"] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df["Adj. Close"].plot()
+df["Forecast"].plot()
+plt.legend(loc=4)
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
