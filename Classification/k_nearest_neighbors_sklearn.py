e774db22b653ec6b12f60e8abc798ac34c39a627
Benjamin Corvera <Benjamin.Corvera@willowtreeapps.com>
2021-10-15 12:11:11 -0400

    cleanup and skearn manual done

--- Classification/k_nearest_neighbors_sklearn.py
+++ Classification/k_nearest_neighbors_sklearn.py
@@ -1,18 +1,19 @@
-### with k nearest neighbors, k should be odd and greater than the number of categories/groups so there are no ties. 
+### with k nearest neighbors, k should be odd and greater than the number of categories/groups so there are no ties.
 # To find distance of points, we use simple euclidian math.
 # The larger the dataset, the worse this algorithm is (scaling is bad). SVM's are much better at classification. ###
 import pandas as pd
 from sklearn import preprocessing, model_selection, neighbors
 import numpy as np
 
-df = pd.read_csv('Classification/breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'],1,inplace=True)
+df = pd.read_csv("Classification/breast-cancer-wisconsin.data")
+df.replace("?", -99999, inplace=True)
+df.drop(["id"], 1, inplace=True)
 
-X = np.array(df.drop(['class'],1))
-y = np.array(df['class'])
+X = np.array(df.drop(["class"], 1))
+X = preprocessing.scale(X)
+y = np.array(df["class"])
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 clf = neighbors.KNeighborsClassifier()
 clf.fit(X_train, y_train)
@@ -22,7 +23,13 @@ print(accuracy)
 
 # this is where you would pickle the classifier
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,2,2,3,2,1],[4,8,1,1,2,2,3,2,1]])
+example_measures = np.array(
+    [
+        [4, 2, 1, 1, 1, 2, 3, 2, 1],
+        [4, 2, 1, 1, 2, 2, 3, 2, 1],
+        [4, 8, 1, 1, 2, 2, 3, 2, 1],
+    ]
+)
 # example_measures = np.reshape(len(example_measures),-1)
 
 prediction = clf.predict(example_measures)
