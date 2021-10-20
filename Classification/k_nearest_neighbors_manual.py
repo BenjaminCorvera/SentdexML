a5f4fcbad613899037c84d37a95fb0a33317f2c1
Benjamin Corvera <Benjamin.Corvera@willowtreeapps.com>
2021-10-20 17:42:04 -0400

    svm from scrath working

--- Classification/k_nearest_neighbors_manual.py
+++ Classification/k_nearest_neighbors_manual.py
@@ -36,10 +36,10 @@ full_data = df.astype(float).values.tolist()
 
 random.shuffle(full_data)
 test_size = 0.4
-train_set = {2:[], 4:[]}
-test_set = {2:[], 4:[]}
-train_data = full_data[:-int(test_size*len(full_data))]
-test_data = full_data[-int(test_size*len(full_data)):]
+train_set = {2: [], 4: []}
+test_set = {2: [], 4: []}
+train_data = full_data[: -int(test_size * len(full_data))]
+test_data = full_data[-int(test_size * len(full_data)) :]
 
 [train_set[i[-1]].append(i[:-1]) for i in train_data]
 [test_set[i[-1]].append(i[:-1]) for i in test_data]
@@ -47,14 +47,13 @@ test_data = full_data[-int(test_size*len(full_data)):]
 correct = 0
 total = 0
 
-for group in test_set: 
-    for data in test_set[group]: 
+for group in test_set:
+    for data in test_set[group]:
         vote, confidence = k_nearest_neighbors(train_set, data, k=5)
         if group == vote:
-            correct +=1
+            correct += 1
         else:
             print(confidence)
-        total +=1
+        total += 1
 
-print('Accuracy', correct/total)
-
+print("Accuracy", correct / total)
